c266206fbfee779c35b82b43be0ff803c08a241a
YueEn Ma <myueen@unc.edu>
2023-09-21 15:46:29 -0400

    test

--- CIR.py
+++ CIR.py
@@ -26,30 +26,58 @@ def CIR(X, Y, Xt, Yt, a, d):
 
     X_df = pd.DataFrame(X)
     Xt_df = pd.DataFrame(Xt)
+    Y_df = pd.DataFrame(Y)
+    Yt_df = pd.DataFrame(Yt)
+    # test: print(Y_df)
 
     # Center the data
     X_colmeans = X_df.sum()/n
-    # test: print(X_colmeans) .
-    # test: print(X_)
+    # test: print(X_colmeans)
     X_ = X_df - X_colmeans
+    # test: print(X_)
 
-    Xt_colmean = Xt_df.sum()/n
+    Xt_colmean = Xt_df.sum()/m
     Xt_ = Xt_df - Xt_colmean
 
     # Covariance Matrix
-    X_cov_matrix = X_.cov()
-    print(X_cov_matrix)
+    X_cov_matrix = (1/n) * np.matmul(X_.transpose(), X_)
+    Xt_cov_matrix = (1/m) * np.matmul(Xt_.transpose(), Xt_)
+    # test: print(X_.transpose())
+    # test: print(X_cov_matrix)
+
+    # Define H, Ht
+    is_numerical = np.issubdtype(Y_df.to_numpy().dtype, np.number)
+    # test: print(Y_df.dtypes)
+    # test: print(is_numerical)
+    is_categorical = not is_numerical
+
+    if is_categorical:
+        H = int(Y_df.nunique()[0])
+        print(H)
+    elif is_numerical:
+        if d <= 2:
+            H = 10
+            # test: print(H)
+        else:
+            H = 4
+            # test: print(H)
+
+
+
+
 
 
 # Test code
 X = [[1, 4, 5],
      [-5, 8, 9]]
 
-Y = [[1], [2]]
+Y_numerical = [[1], [2]]
+
+Y_categorical = ["a", "b"]
 
 Xt = [[1, 4, 5],
       [-5, 8, 9]]
 
 Yt = [[1], [2]]
 
-CIR(X, Y, Xt, Yt, 1, 3)
+CIR(X, Y_categorical, Xt, Yt, 1, 2)

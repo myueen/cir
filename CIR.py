a6274fdb0204260ef1843de94ae80ef96fb7d93e
YueEn Ma <myueen@unc.edu>
2023-10-09 08:57:50 -0400

    test

--- CIR.py
+++ CIR.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from scipy.linalg import eig
+from scipy.linalg import eigh
 from scipy.optimize import minimize
 from pymanopt.manifolds import Stiefel
 from pymanopt import Problem
@@ -19,13 +19,13 @@ def CIR(X, Y, Xt, Yt, a, d):
         raise ValueError("Y must have the same number of rows as X")
 
     if len(Yt) != m:
-        raise ValueError("Yt must have the same number of rows as X")
+        raise ValueError("Yt must have the same number of rows as Xt")
 
     if not isinstance(d, int):
         raise TypeError("d parameter must be an integer")
 
-    if d < 0:
-        raise ValueError("d must be greater than or equal to 0")
+    if d < 1:
+        raise ValueError("d must be greater than or equal to 1")
 
     if a < 0:
         raise ValueError("a must be greater than or equal to 0")
@@ -56,48 +56,40 @@ def CIR(X, Y, Xt, Yt, a, d):
 
     # Define Ph     (Count the # of ocurrence of y in each H interval)
     interval_Ph = pd.cut(Y_df[0], bins=H)
-    Ph = interval_Ph.value_counts().sort_index()  # interval_counts
+    Ph = interval_Ph.value_counts().sort_index()
 
     # Find the mean: Take each row of X and average among each interval separately
     interval = np.linspace(np.min(Y), np.max(Y), num=H+1)
-    # masks = [(Y >= interval[i]) & (Y <= interval[i+1]) for i in range(H)]
-    # mh = [np.nanmean(X_centered[mask], axis=0) if np.any(
-    #     mask) else np.full(X_centered.shape[1], np.nan) for mask in masks]
-    # mh_df = pd.DataFrame(mh)
-    # print(mh_df)
 
     mh = []
     for i in range(len(interval) - 1):
         mask = (Y >= interval[i]) & (Y <= interval[i+1])
         x_rows_mean = np.mean(X_centered[mask], axis=0)
         mh.append(x_rows_mean)
-    mh_df = pd.DataFrame(mh)
+
+    mh_array = np.array(mh)
 
     # Cov(E[X|Y])
     sigma_X = np.zeros((X_centered.shape[1], X_centered.shape[1]))
     for i in range(len(interval) - 1):
         mask = (Y >= interval[i]) & (Y <= interval[i+1])
-        interval_mh = mh[i]
+        interval_mh = mh_array[i]
         outer_product = np.outer(interval_mh, interval_mh)
-        sigma_X += outer_product
 
-    sigma_X_df = pd.DataFrame(sigma_X)
-    print(X_cov_matrix)
-    print(sigma_X_df)
+        if not np.isnan(outer_product).any():
+            sigma_X += outer_product
+
+    sigma_X_a = np.array(sigma_X)
+    X_cov_matrix_a = np.array(X_cov_matrix)
 
     # Generalized Eigenvalue Decomposition
-    A = np.dot(np.dot(X_cov_matrix, sigma_X_df), X_cov_matrix)
-    B = np.dot(X_cov_matrix, X_cov_matrix)
-    bv_inverse = np.lingalg.inv(np.dot(np.dot(v.T, B), v))
-    va = np.dot(np.dot(v.T, A), v)
-    trace_ab = np.dot(va, bv_inverse)
+    A = np.matmul(np.matmul(X_cov_matrix_a, sigma_X_a), X_cov_matrix_a)
+    B = np.matmul(X_cov_matrix_a, X_cov_matrix_a)
 
     if a == 0:
-        v = eig(X_cov_matrix, sigma_X_df)
-        v_df = pd.DataFrame(v)
-        print(v_df)
-        f_v = -1 * (np.trace(trace_ab))
-        return v, f_v
+        eigenvalues, eigenvectors = eigh(X_cov_matrix_a, sigma_X_a)
+        v = eigenvectors[:d, :]
+        return v, f(A, B, a, v, 0, 0)
 
     # ====================================================================================
     # ====================================================================================
@@ -135,25 +127,27 @@ def CIR(X, Y, Xt, Yt, a, d):
         mask_t = (Yt >= interval_t[i]) & (Yt <= interval_t[i+1])
         interval_mh_t = mh_t[i]
         outer_product_t = np.outer(interval_mh_t, interval_mh_t)
+        if not np.isnan(outer_product_t).any():
+            sigma_Xt += outer_product_t
         sigma_Xt += outer_product_t
 
     # f(v)
     sigma_Xt_df = pd.DataFrame(sigma_Xt)
-    At = np.dot(np.dot(Xt_cov_matrix, sigma_Xt_df), Xt_cov_matrix)
-    Bt = np.dot(Xt_cov_matrix, Xt_cov_matrix)
-    bv_inverse_t = np.lingalg.inv(np.dot(np.dot(v.T, Bt), v))
-    va_t = np.dot(np.dot(v.T, At), v)
-    trace_ab_t = np.dot(va_t, bv_inverse_t)
+    At = np.matmul(np.matmul(Xt_cov_matrix, sigma_Xt_df), Xt_cov_matrix)
+    Bt = np.matmul(Xt_cov_matrix, Xt_cov_matrix)
+    bv_inverse_t = np.linalg.inv(np.matmul(np.matmul(v.T, Bt), v))
+    va_t = np.matmul(np.matmul(v.T, At), v)
+    trace_ab_t = np.matmul(va_t, bv_inverse_t)
 
     # gradient f(v)
-    avb = np.dot(np.dot(A, v), bv_inverse) - \
-        np.dot(np.dot(np.dot(np.dot(B, v), bv_inverse), va), bv_inverse)
-    avb_t = np.dot(np.dot(At, v), bv_inverse_t) - \
-        np.dot(np.dot(np.dot(np.dot(Bt, v), bv_inverse_t), va_t), bv_inverse_t)
-    grad = -2 * (avb - a * (avb_t))
+    # avb = np.dot(np.dot(A, v), bv_inverse) - \
+    #     np.dot(np.dot(np.dot(np.dot(B, v), bv_inverse), va), bv_inverse)
+    # avb_t = np.dot(np.dot(At, v), bv_inverse_t) - \
+    #     np.dot(np.dot(np.dot(np.dot(Bt, v), bv_inverse_t), va_t), bv_inverse_t)
+    # grad = -2 * (avb - a * (avb_t))
 
-    if a > 0:
-        f_v = -1 * (np.trace(trace_ab)) + a * (np.trace(trace_ab_t))
+    # if a > 0:
+    # f_v = -1 * (np.trace(trace_ab)) + a * (np.trace(trace_ab_t))
 
     # ====================================================================================
     # ====================================================================================
@@ -178,6 +172,15 @@ def CIR(X, Y, Xt, Yt, a, d):
     # print("             ")
     # print("Intervals and count: ")
     # print(Ph)
+
+
+def f(A, B, a, v, At=0, Bt=0):
+    bv_inverse = np.linalg.inv(np.matmul(np.matmul(v.T, B), v))
+    va = np.matmul(np.matmul(v.T, A), v)
+    f_v = -1 * (np.trace(np.matmul(va, bv_inverse)))
+    return f_v
+
+
 # Test code
 X = [[1, 4, 5],
      [-5, 8, 9],
@@ -191,8 +194,14 @@ X2 = [[55, 28, 34],
       [30, 32, 35],
       [45, 29, 28]]
 
+X3 = [[120, 28, 34],
+      [30, 98, 35],
+      [45, 103, 28],
+      [33, 62, 59],
+      [89, 75, 28]]
+
 Y_numerical = [1, 8.3, 2.7]
-Y1 = [1, 13, 20]
+Y1 = [1, 13, 20, 55, 23]
 Y_binary = [0, 1, 1]
 Y_unique_less_10 = [1, 2, 3]
 Y_categorical = ["a", "b", "b"]
@@ -202,4 +211,7 @@ Xt = [[1, 4, 5],
 
 Yt = [[1], [2]]
 
-CIR(X2, Y1, Xt, Yt, 0, 2)
+result = CIR(X3, Y1, Xt, Yt, 0, 3)
+# print(result)
+# print(result[0])
+# print(result[1])

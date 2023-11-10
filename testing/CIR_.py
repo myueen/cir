b0a7989644dcedbb253b5b5ec40ee996f5ff1b91
YueEn Ma <myueen@unc.edu>
2023-11-10 15:54:52 -0500

    test

--- testing/CIR_.py
+++ testing/CIR_.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from scipy.linalg import eigh
+from scipy.linalg import eig
 import torch
 import geoopt
 from geoopt.optim import RiemannianSGD
@@ -12,11 +13,20 @@ def CIR(X, Y, Xt, Yt, a, d):
     Y = pd.DataFrame(Y)
     Yt = pd.DataFrame(Yt)
 
-    if X.iloc[:, 0].dtypes != X.iloc[:, 1].dtypes:
+    # check whether the first column is sequential numbers
+    if X.iloc[:, 0].equals(pd.Series(range(1, len(X) + 1))):
         X = X.iloc[:, 1:]
 
-    # n represents the rows of X. p represents the
-    # columns of X. m represent the rows of Xt.
+    if Xt.iloc[:, 0].equals(pd.Series(range(1, len(Xt) + 1))):
+        Xt = Xt.iloc[:, 1:]
+
+    if Y.iloc[:, 0].equals(pd.Series(range(1, len(Y) + 1))):
+        Y = Y.iloc[:, 1:]
+
+    if Yt.iloc[:, 0].equals(pd.Series(range(1, len(Yt) + 1))):
+        Yt = Yt.iloc[:, 1:]
+
+    # n represents the rows of X. p represents the columns of X. m represent the rows of Xt.
     n = len(X)
     p = len(X.columns)
     m = len(Xt)
@@ -40,19 +50,18 @@ def CIR(X, Y, Xt, Yt, a, d):
     if a < 0:
         raise ValueError('a must be greater than or equal to 0')
 
-    # Center the matrix X by subtracting the
-    # original matrix X by the column means of X
-    X_col_means = X.mean(axis=0)
-    X = X - X_col_means
+    # Center the matrix X by subtracting the original matrix X by the column means of X
+    col_mean_x = X.mean(axis=0)
+    X = X - col_mean_x
 
     # Calculate the Covariance Matrix
-    X_cov_matrix = X.cov()
+    cov_matrix_X = X.cov()
 
     # Define H
     Y_unique_value = Y.nunique().item()
     if Y_unique_value == 2:
         H = 2
-    elif Y_unique_value > 2 & Y_unique_value <= 10:
+    elif 2 < Y_unique_value <= 10:
         H = Y_unique_value
     else:
         if d <= 2:
@@ -60,167 +69,163 @@ def CIR(X, Y, Xt, Yt, a, d):
         else:
             H = 4
 
-    print(H)
+    # Define Ph, count the of ocurrence of y in each H interval.
+    interval_Ph = pd.cut(Y.iloc[:, 0], bins=H)
+    Ph = interval_Ph.value_counts().sort_index()
 
-    # Define Ph. Count the # of ocurrence of y in each H interval.
-    # interval_Ph = pd.cut(Y[0], bins=H)
-    # Ph = interval_Ph.value_counts().sort_index()
+    # Find the mean: Take each row of X and average among each interval separately
+    interval = np.linspace(np.min(Y), np.max(Y), num=H+1)
+    mh = []
+    for i in range(len(interval) - 1):
+        mask = (interval[i] <= Y) & (Y <= interval[i+1])
+        mask = mask.squeeze()  # convert to Series
+        x_rows_mean = np.mean(X[mask.values], axis=0)
+        mh.append(x_rows_mean)
 
-    # # Find the mean: Take each row of X and average among each interval separately
-    # interval = np.linspace(np.min(Y), np.max(Y), num=H+1)
+    mh = np.array(mh)
 
-    # mh = []
-    # for i in range(len(interval) - 1):
-    #     mask = (Y >= interval[i]) & (Y <= interval[i+1])
-    #     x_rows_mean = np.mean(X_centered[mask], axis=0)
-    #     mh.append(x_rows_mean)
+    # Cov(E[X|Y])
+    sigma_X = np.zeros((X.shape[1], X.shape[1]))
+    for i in range(len(interval) - 1):
+        mask = (interval[i] <= Y) & (Y <= interval[i+1])
+        interval_mh = mh[i]
+        outer_product = np.outer(interval_mh, interval_mh)
 
-    # mh_array = np.array(mh)
+        if not np.isnan(outer_product).any():
+            sigma_X += outer_product
 
-    # # Cov(E[X|Y])
-    # sigma_X = np.zeros((X_centered.shape[1], X_centered.shape[1]))
-    # for i in range(len(interval) - 1):
-    #     mask = (Y >= interval[i]) & (Y <= interval[i+1])
-    #     interval_mh = mh_array[i]
-    #     outer_product = np.outer(interval_mh, interval_mh)
+    sigma_X = np.array(sigma_X)
+    sigma_X = sigma_X + (5*10 ** (-14))*np.identity(p)
+    # eigenvalues, eigenvectors = np.linalg.eigh(sigma_X)
+    # print(eigenvalues)
+    cov_matrix_X = np.array(cov_matrix_X)
 
-    #     if not np.isnan(outer_product).any():
-    #         sigma_X += outer_product
+    # Generalized Eigenvalue Decomposition
+    A = cov_matrix_X @ sigma_X @ cov_matrix_X
+    B = cov_matrix_X @ cov_matrix_X
+    eigenvalues, eigenvectors = eigh(cov_matrix_X, sigma_X)
 
-    # sigma_X_a = np.array(sigma_X)
-    # X_cov_matrix_a = np.array(X_cov_matrix)
+    if a == 0:
+        v = eigenvectors[:d, :]
+        f_v = -1 * (np.trace(v.T @ A @ v @ np.linalg.inv(v.T @ B @ v)))
+        return v, f_v
+    # =======================================================================================
+    # Background data and the case when a > 0
 
-    # # Generalized Eigenvalue Decomposition
-    # A = X_cov_matrix_a @ sigma_X_a @ X_cov_matrix_a
-    # B = X_cov_matrix_a @ X_cov_matrix_a
-    # eigenvalues, eigenvectors = eigh(X_cov_matrix_a, sigma_X_a)
+    # Center the data
+    col_means_xt = Xt.mean(axis=0)
+    Xt = Xt - col_means_xt
 
-#     if a == 0:
-#         v = eigenvectors[:d, :]
-#         f_v = -1 * (np.trace(v.T @ A @ v @ np.linalg.inv(v.T @ B @ v)))
-#         return v, f_v
-#     # =======================================================================================
-#     # Background data and the case when a > 0
+    # Covariance Matrix
+    cov_matrix_Xt = Xt.cov()
 
-#     # Center the data
-#     Xt_col_means = Xt_df.mean(axis=0)
-#     Xt_centered = Xt_df - Xt_col_means
+    # Define Ht
+    Yt_unique_value = Yt.nunique().item()
+    if Yt_unique_value == 2:
+        Ht = 2
+    elif 2 < Yt_unique_value <= 10:
+        Ht = Yt_unique_value
+    else:
+        if d <= 2:
+            Ht = 10
+        else:
+            Ht = 4
 
-#     # Covariance Matrix
-#     Xt_cov_matrix = Xt_centered.cov()
+    # Define Ph_t
+    interval_Ph_t = pd.cut(Yt.iloc[:, 0], bins=Ht)
+    Ph_t = interval_Ph_t.value_counts().sort_index()
 
-#     # Define Ht
-#     Yt_unique_value = Yt_df.nunique().item()
-#     if Yt_unique_value == 2:
-#         Ht = 2
-#     elif Yt_unique_value > 2 & Yt_unique_value <= 10:
-#         Ht = Yt_unique_value
-#     else:
-#         if d <= 2:
-#             Ht = 10
-#         else:
-#             Ht = 4
+    # Find the mean: Take each row of X and average among each interval separately
+    interval_t = np.linspace(np.min(Yt), np.max(Yt), num=Ht+1)
+    mh_t = []
+    for i in range(len(interval_t) - 1):
+        mask_t = (interval_t[i] <= Yt) & (Yt <= interval_t[i+1])
+        mask = mask.squeeze()
+        xt_rows_mean = np.mean(Xt[mask_t.values], axis=0)
+        mh_t.append(xt_rows_mean)
 
-#     # Define Ph_t
-#     interval_Ph_t = pd.cut(Yt_df[0], bins=Ht)
-#     Ph_t = interval_Ph_t.value_counts().sort_index()
+    mh_t = np.array(mh_t)
 
-#     # Find the mean: Take each row of X and average among each interval separately
-#     interval_t = np.linspace(np.min(Yt), np.max(Yt), num=Ht+1)
-#     mh_t = []
+    # Cov(E[Xt|Yt])
+    sigma_Xt = np.zeros((Xt.shape[1], Xt.shape[1]))
+    for i in range(len(interval_t) - 1):
+        mask_t = (interval_t[i] <= Yt) & (Yt <= interval_t[i+1])
+        interval_mh_t = mh_t[i]
+        outer_product_t = np.outer(interval_mh_t, interval_mh_t)
 
-#     for i in range(len(interval_t) - 1):
-#         mask_t = (Yt >= interval_t[i]) & (Yt <= interval_t[i+1])
-#         xt_rows_mean = np.mean(Xt_centered[mask_t], axis=0)
-#         mh_t.append(xt_rows_mean)
+        if not np.isnan(outer_product_t).any():
+            sigma_Xt += outer_product_t
 
-#     mh_t_array = np.array(mh_t)
+    sigma_Xt = np.array(sigma_Xt)
+    cov_matrix_Xt = np.array(cov_matrix_Xt)
 
-#     # Cov(E[Xt|Yt])
-#     sigma_Xt = np.zeros((Xt_centered.shape[1], Xt_centered.shape[1]))
-#     for i in range(len(interval_t) - 1):
-#         mask_t = (Yt >= interval_t[i]) & (Yt <= interval_t[i+1])
-#         interval_mh_t = mh_t_array[i]
-#         outer_product_t = np.outer(interval_mh_t, interval_mh_t)
+    # Generalized Eigenvalue Decomposition
+    At = cov_matrix_Xt @ sigma_Xt @ cov_matrix_Xt
+    Bt = cov_matrix_Xt @ cov_matrix_Xt
 
-#         if not np.isnan(outer_product_t).any():
-#             sigma_Xt += outer_product_t
+    # Optimization on Manifold
+    stiefel = geoopt.manifolds.Stiefel()
+    initial_guess = torch.randn(p, d)
+    initial_guess, _ = torch.linalg.qr(initial_guess)
 
-#     sigma_Xt_a = np.array(sigma_Xt)
-#     Xt_cov_matrix_a = np.array(Xt_cov_matrix)
+    v = geoopt.ManifoldParameter(initial_guess, manifold=stiefel)
+    optimizer = RiemannianSGD([v], lr=0.1)
+    v = v.to(torch.double)
 
-#     # Generalized Eigenvalue Decomposition
-#     At = Xt_cov_matrix_a @ sigma_Xt_a @ Xt_cov_matrix_a
-#     Bt = Xt_cov_matrix_a @ Xt_cov_matrix_a
+    for step in range(100000):
+        optimizer.zero_grad()
+        gradient = grad(A, B, a, v, At, Bt)
+        proj_grad = stiefel.proju(v, gradient)
+        v.grad = proj_grad
+        # v.manifold.proju(v, gradient)
+        cost = f(A, B, a, v, At, Bt)
+        optimizer.step()
 
-#     # Optimization on Manifold
-#     stiefel = geoopt.manifolds.Stiefel()
-#     initial_guess = torch.randn(p, d)
-#     initial_guess, _ = torch.linalg.qr(initial_guess)
+    print(v @ v.t())
 
-#     v = geoopt.ManifoldParameter(initial_guess, manifold=stiefel)
-#     optimizer = RiemannianSGD([v], lr=0.1)
+    # print(v.shape)
 
-#     v = v.to(torch.double)
-
-#     for step in range(100):
-#         optimizer.zero_grad()
-#         gradient = grad(A, B, a, v, At, Bt)
-#         proj_grad = stiefel.proju(v, gradient)
-#         v.grad = proj_grad
-#         # v.manifold.proju(v, gradient)
-#         cost = f(A, B, a, v, At, Bt)
-#         optimizer.step()
-
-#     optimized_matrix = v
-
-#     print("Optimized Orthonormal Matrix: ")
-#     print(optimized_matrix)
-
-#     optimized_tensor = v.clone().detach()
-#     print("Optimized Orthonormmal Matrix (as PyTorch tensor):")
-#     print(optimized_tensor)
 
 #     return v
 
 
-# def f(A, B, a, v, At, Bt):
-#     A = torch.tensor(A)
-#     B = torch.tensor(B)
-#     At = torch.tensor(At)
-#     Bt = torch.tensor(Bt)
+def f(A, B, a, v, At, Bt):
+    A = torch.tensor(A)
+    B = torch.tensor(B)
+    At = torch.tensor(At)
+    Bt = torch.tensor(Bt)
 
-#     bv_inv = torch.inverse(torch.matmul(torch.matmul(v.t(), B), v))
-#     va = torch.matmul(torch.matmul(v.T, A), v)
-#     bv_t_inv = torch.inverse(torch.matmul(torch.matmul(v.t(), Bt), v))
-#     va_t = torch.matmul(torch.matmul(v.T, At), v)
+    bv_inv = torch.inverse(torch.matmul(torch.matmul(v.t(), B), v))
+    va = torch.matmul(torch.matmul(v.T, A), v)
+    bv_t_inv = torch.inverse(torch.matmul(torch.matmul(v.t(), Bt), v))
+    va_t = torch.matmul(torch.matmul(v.T, At), v)
 
-#     f_v = -torch.trace(torch.matmul(va, bv_inv)) + a * \
-#         torch.trace(torch.matmul(va_t, bv_t_inv))
+    f_v = -torch.trace(torch.matmul(va, bv_inv)) + a * \
+        torch.trace(torch.matmul(va_t, bv_t_inv))
 
-#     return f_v
+    return f_v
 
 
-# def grad(A, B, a, v, At, Bt):
-#     A = torch.tensor(A)
-#     B = torch.tensor(B)
-#     At = torch.tensor(At)
-#     Bt = torch.tensor(Bt)
+def grad(A, B, a, v, At, Bt):
+    A = torch.tensor(A)
+    B = torch.tensor(B)
+    At = torch.tensor(At)
+    Bt = torch.tensor(Bt)
 
-#     v = v.to(torch.double)
-#     A = A.to(torch.double)
-#     B = B.to(torch.double)
-#     At = At.to(torch.double)
-#     Bt = Bt.to(torch.double)
+    v = v.to(torch.double)
+    A = A.to(torch.double)
+    B = B.to(torch.double)
+    At = At.to(torch.double)
+    Bt = Bt.to(torch.double)
 
-#     bv_inv = torch.inverse(v.t() @ B @ v)
-#     va = v.t() @ A @ v
-#     bv_t_inv = torch.inverse(v.t() @ Bt @ v)
-#     va_t = v.t() @ At @ v
+    bv_inv = torch.inverse(v.t() @ B @ v)
+    va = v.t() @ A @ v
+    bv_t_inv = torch.inverse(v.t() @ Bt @ v)
+    va_t = v.t() @ At @ v
 
-#     avb = A @ v @ bv_inv - B @ v @ bv_inv @ va @ bv_inv
-#     avb_t = At @ v @ bv_t_inv - Bt @ v @ bv_t_inv @ va_t @ bv_t_inv
+    avb = A @ v @ bv_inv - B @ v @ bv_inv @ va @ bv_inv
+    avb_t = At @ v @ bv_t_inv - Bt @ v @ bv_t_inv @ va_t @ bv_t_inv
 
-#     gradient = -2 * (avb - a * avb_t)
+    gradient = -2 * (avb - a * avb_t)
 
-#     return gradient
+    return gradient

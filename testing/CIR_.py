37ad77a4b7445ab5d4c3c6a56982f13ada050a57
YueEn Ma <myueen@unc.edu>
2023-12-15 22:20:14 -0500

    test

--- testing/CIR_.py
+++ testing/CIR_.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import math
 from scipy.linalg import eigh
 from scipy.linalg import eig
 from numpy.linalg import norm
@@ -7,9 +8,11 @@ import torch
 import geoopt
 from geoopt.optim import RiemannianSGD
 from geoopt.manifolds import Stiefel
+from scipy.linalg import solve
+from scipy.linalg import cholesky
 
 
-def CIR(X, Y, Xt, Yt, a, d):
+def CIR(X, Y, Xt, Yt, a, d, opt_option):
     X = pd.DataFrame(X)
     Xt = pd.DataFrame(Xt)
     Y = pd.DataFrame(Y)
@@ -169,40 +172,50 @@ def CIR(X, Y, Xt, Yt, a, d):
     At = cov_matrix_Xt @ sigma_Xt @ cov_matrix_Xt
     Bt = cov_matrix_Xt @ cov_matrix_Xt
 
-    A = torch.tensor(A, dtype=torch.double)
-    B = torch.tensor(B, dtype=torch.double)
-    a = torch.tensor(a, dtype=torch.double)
-    At = torch.tensor(At, dtype=torch.double)
-    Bt = torch.tensor(Bt, dtype=torch.double)
-
     # Optimization on Manifold
-    stiefel = geoopt.manifolds.Stiefel()
-    torch.manual_seed(2)
-    initial_point = torch.randn(p, d)
-    initial_point, _ = torch.linalg.qr(initial_point)
+    if opt_option == "geoopt":
+        A = torch.tensor(A, dtype=torch.double)
+        B = torch.tensor(B, dtype=torch.double)
+        a = torch.tensor(a, dtype=torch.double)
+        At = torch.tensor(At, dtype=torch.double)
+        Bt = torch.tensor(Bt, dtype=torch.double)
 
-    v = geoopt.ManifoldParameter(initial_point, manifold=stiefel)
+        stiefel = geoopt.manifolds.Stiefel()
+        torch.manual_seed(1)
+        initial_point = torch.randn(p, d)
+        initial_point, _ = torch.linalg.qr(initial_point)
 
-    optimizer = RiemannianSGD([v], lr=1e-3, momentum=0.9)
-    max_iterations = 10000
+        v = geoopt.ManifoldParameter(initial_point, manifold=stiefel)
 
-    for step in range(max_iterations):
-        vt = v.clone()
-        optimizer.zero_grad()
-        cost = f(A, B, a, v, At, Bt)
-        gradient = grad(A, B, a, v, At, Bt).to(torch.float)
-        v.grad = gradient
-        optimizer.step()
-        vt_plus = v.clone()
+        optimizer = RiemannianSGD([v], lr=1e-3, momentum=0.9)
+        max_iterations = 10000
 
-        if stepExit(vt_plus, vt, cost, A, B, At, Bt, a):
-            break
+        for step in range(max_iterations):
+            vt = v.clone()
+            optimizer.zero_grad()
+            cost = f_geoopt(A, B, a, v, At, Bt)
+            gradient = grad_geoopt(A, B, a, v, At, Bt).to(torch.float)
+            v.grad = gradient
+            optimizer.step()
+            vt_plus = v.clone()
 
-    output = v @ v.t()
-    return output
+            if stepExit(vt_plus, vt, cost, A, B, At, Bt, a):
+                break
+
+        output = v @ v.t()
+        return output
+
+    if opt_option == "SGPM":
+        np.random.seed(2)
+        v = np.random.rand(p, d)
+        v, r = np.linalg.qr(v)
+
+        output = SGPM(v, A, B, At, Bt, a)
+        # return output
+        return output @ np.transpose(output)
 
 
-def f(A, B, a, v, At, Bt):
+def f_geoopt(A, B, a, v, At, Bt):
     v = v.data
     v = v.to(torch.double)
 
@@ -215,7 +228,7 @@ def f(A, B, a, v, At, Bt):
     return f_v
 
 
-def grad(A, B, a, v, At, Bt):
+def grad_geoopt(A, B, a, v, At, Bt):
     v = v.data
     v = v.to(torch.double)
 
@@ -232,18 +245,238 @@ def grad(A, B, a, v, At, Bt):
 
 
 def stepExit(vt_plus, vt, cost, A, B, At, Bt, a) -> bool:
-    xtol = 1e-6  # 0.025
+    xtol = 1e-6
     gtol = 1e-4
     ftol = 1e-12
-    distance = torch.norm(vt_plus @ vt_plus.t() - vt @ vt.t(), "fro")
+    distance = torch.norm(vt_plus @ vt_plus.t() - vt @ vt.t(), 'fro')
     vt_plus_gradient = grad(A, B, a, vt_plus, At, Bt)
     cost_vt_plus = f(A, B, a, vt_plus, At, Bt)
 
     if distance < xtol:
         return True
-    elif torch.norm(vt_plus_gradient, "fro") < gtol:
+    elif torch.norm(vt_plus_gradient, 'fro') < gtol:
         return True
     elif abs(cost_vt_plus - cost) < ftol:
         return True
     else:
         return False
+
+
+def f(A, B, a, v, At, Bt):
+    bv_inv = np.linalg.inv(np.transpose(v) @ B @ v)
+    va = np.transpose(v) @ A @ v
+    bv_t_inv = np.linalg.inv(np.transpose(v) @ Bt @ v)
+    va_t = np.transpose(v) @ At @ v
+    f_v = -np.matrix.trace(va @ bv_inv) + a * np.matrix.trace(va_t @ bv_t_inv)
+    return f_v
+
+
+def grad(A, B, a, v, At, Bt):
+    bv_inv = np.linalg.inv(np.transpose(v) @ B @ v)
+    va = np.transpose(v) @ A @ v
+    bv_t_inv = np.linalg.inv(np.transpose(v) @ Bt @ v)
+    va_t = np.transpose(v) @ At @ v
+
+    avb = A @ v @ bv_inv - B @ v @ bv_inv @ va @ bv_inv
+    avb_t = At @ v @ bv_t_inv - Bt @ v @ bv_t_inv @ va_t @ bv_t_inv
+
+    gradient = -2 * (avb - a * avb_t)
+    return gradient
+
+
+def SGPM(X, A, B, At, Bt, a):
+    X = np.array(X)
+    n, k = X.shape
+
+    xtol = 1e-6
+    gtol = 1e-4
+    ftol = 1e-12
+    rho = 1e-4
+    eta = 0.1
+    gamma = 0.85
+    tau = 1e-3
+    STPEPS = 1e-10
+    nt = 5
+    mxitr = 1000
+    alpha = 0.85
+    record = 0
+    projG = 2
+    iscomplex = 0
+    crit = np.ones((mxitr, 3))
+    invH = True
+
+    if k < n/2:
+        invH = False
+        eye2k = np.identity(2 * k)
+
+    # Initial function value and gradient
+    # Prepare for iterations
+    F = f(A, B, a, X, At, Bt)
+    G = grad(A, B, a, X, At, Bt)
+
+    GX = np.transpose(G) @ X
+
+    if invH:
+        GXT = G @ np.transpose(X)
+        H = GXT - np.transpose(GXT)
+    else:
+        if projG == 1:
+            U = np.hstack((G, X))
+            V = np.hstack((X, -G))
+            VU = np.transpose(V) @ U
+        elif projG == 2:
+            GB = G - 0.5 * X @ (np.transpose(X) @ G)
+            U = np.hstack((GB, X))
+            V = np.hstack((X, -GB))
+            VU = np.transpose(V) @ U
+        VX = np.transpose(V) @ X
+
+    dtX = G - X @ GX
+    nrmG = np.linalg.norm(dtX, ord='fro')
+    Q = 1
+    Cval = F
+
+    # main iteration
+    F_eval = np.zeros((mxitr + 1, 1))
+    Grad = np.zeros((mxitr + 1, 1))
+    F_eval[0] = F
+    Grad[0] = nrmG
+    for itr in range(0, mxitr):
+        XP, FP, dtXP, nrmGP = X, F, dtX, nrmG
+
+        # scale step size
+        nls = 1
+        deriv = rho * (nrmG**2)
+
+        while True:
+            # Update Scheme
+            if invH:
+                if abs(alpha) < rho:    # Explicit Euler (Steepest Descent)
+                    X = XP - tau * dtXP
+                elif abs(alpha - 0.5) < rho:  # Crank-Nicolson
+                    A = np.eye(n) + (tau * 0.5) * H
+                    X = solve(
+                        A, XP - (0.5 * tau) * dtXP, lower=False)
+                elif abs(alpha - 1) < rho:  # Implicit EuLer
+                    A = np.eye(n) + tau * H
+                    X = solve(A, XP, lower=False)
+                else:  # Convex Combination
+                    A = np.eye(n) + (tau * alpha) * H
+                    X = solve(
+                        A, XP - ((1 - alpha) * tau) * dtXP, lower=False)
+
+                if abs(alpha - 0.5) > rho:
+                    XtX = np.transpose(X) @ X
+                    L = cholesky(XtX, lower=False)
+                    X = X @ np.linalg.inv(L)
+
+            else:
+                aa = solve(eye2k + (alpha * tau) * VU, VX)
+                X = XP - U @ (tau * aa)
+
+                if abs(alpha - 0.5) > rho:
+                    XtX = np.transpose(X) @ X
+                    L = cholesky(XtX, lower=False)
+                    X = X @ np.linalg.inv(L)
+
+            # calculate G, F
+            F = f(A, B, a, X, At, Bt)
+            G = grad(A, B, a, X, At, Bt)
+
+            if F <= Cval - tau * deriv or nls >= 5:
+                break
+
+            tau = eta * tau
+            nls += 1
+
+        GX = np.transpose(G) @ X
+        dtX = G - X @ GX
+        nrmG = np.linalg.norm(dtX, ord='fro')
+
+        F_eval[itr+1] = F
+        # print(Grad)
+        Grad[itr+1] = nrmG
+
+        # Adaptive scaling matrix strategy
+        if nrmG < nrmGP:
+            if nrmG >= 0.5 * nrmGP:
+                alpha = max(min(alpha * 1.1, 1), 0)
+        else:
+            alpha = max(min(alpha * 0.9, 0), 0.5)
+
+        # Computing the Riemannian Gradient
+        if invH:
+            if abs(alpha) > rho:
+                GXT = G @ np.transpose(X)
+                H = GXT - np.transpose(GXT)
+        else:
+            if projG == 1:
+                U = np.hstack((G, X))
+                V = np.hstack((X, -G))
+                VU = np.transpose(V) @ U
+            elif projG == 2:
+                GB = G - X @ (0.5 * np.transpose(GX))
+                U = np.hstack((GB, X))
+                V = np.hstack((X, -GB))
+                VU = np.transpose(V) @ U
+            VX = np.transpose(V) @ X
+
+        # Compute the Alternate ODH step-size
+        S = X - XP
+        SS = np.sum(np.sum(np.multiply(S, S), axis=1), axis=0)
+
+        XDiff = math.sqrt(SS / n)
+        FDiff = abs(FP - F) / (abs(FP) + 1)
+
+        Y = dtX - dtXP
+        SY = abs(np.sum(np.sum(np.multiply(S, Y), axis=1), axis=0))
+
+        if itr % 2 == 0:
+            tau = SS / SY
+        else:
+            YY = np.sum(np.sum(np.multiply(Y, Y), axis=1), axis=0)
+            tau = SY / YY
+
+        tau = max(min(tau, 1e20), 1e-20)
+
+        # Stopping Rules
+        crit[itr, :] = [nrmG, XDiff, FDiff]
+
+        mcrit = np.mean(crit[max(0, itr-nt+1):itr, :], axis=0)
+        # mcrit = np.mean(crit[itr - min(nt, itr) + 1:itr, :], axis=0)
+        # print("Iteration:", itr)
+        # print("size of crit", crit.shape)
+        # print("size of mcrit", mcrit.shape)
+
+        if (XDiff < xtol and FDiff < ftol) or nrmG < gtol or all(mcrit[1:3] < 10 * np.hstack((xtol, ftol))):
+            if itr <= 2:
+                ftol = 0.1 * ftol
+                xtol = 0.1 * xtol
+                gtol = 0.1 * gtol
+            else:
+                msg = "converge"
+                break
+
+        Qp = Q
+        Q = gamma * Qp + 1
+        Cval = (gamma * Qp * Cval + F) / Q
+
+    F_eval = F_eval[0:itr, 0]
+    Grad = Grad[0:itr, 0]
+
+    if itr >= mxitr:
+        msg = "exceed max iteration"
+
+    feasi = np.linalg.norm(np.transpose(X) @ X - np.eye(k), 'fro')
+
+    if feasi > 1e-13:
+        L = np.linalg.cholesky(X.T @ X)
+        X = X @ np.linalg.inv(L)
+        F = f(A, B, a, X, At, Bt)
+        G = grad(A, B, a, X, At, Bt)
+        dtX = G - X @ GX
+        nrmG = np.linalg.norm(dtX, 'fro')
+        feasi = np.linalg.norm(X.T @ X - np.eye(k), 'fro')
+
+    feasi = np.linalg.norm(X.T @ X - np.eye(k), 'fro')
+    return X

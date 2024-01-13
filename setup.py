fe95d3f7efd3258b8187eb48256ebbe9d0a990da
YueEn Ma <myueen@unc.edu>
2024-01-12 20:18:45 -0500

    add functiono in init file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cir',
-    version='0.1',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'numpy>=1.24.3',

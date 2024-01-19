8970bda5a8f74ca268270ea6f2290baedda4ec13
YueEn Ma <myueen@unc.edu>
2024-01-18 20:23:52 -0500

    change package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='contrastive_inverse_regression',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     install_requires=[
         'numpy>=1.24.3',

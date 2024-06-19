d98c21aebc850167503eca3113184a41bb43329d
YueEn Ma <myueen@unc.edu>
2024-06-19 13:35:29 -0400

    publish new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='contrastive_inverse_regression',
-    version='1.0.6',
+    version='1.0.7',
     packages=find_packages(),
     # install_requires=[
     #     'numpy>=1.0.3',
